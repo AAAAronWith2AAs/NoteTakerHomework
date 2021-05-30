30f9cee7c217d825788c503d99b5c7e327f07b94
Aaron Mendoza <aaronmendoza@Aarons-iMac.local>
2021-05-30 12:43:22 -0600

    first page connect

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,11 +3,11 @@ const path = require("path");
 const router = require("express").Router();
 
 router.get("/notes", (req, res) => {
-  res.sendFile(path.join(__dirname__, "../public/notes.html")); //localhost:3000/notes extension
+  res.sendFile(path.join(__dirname, "../public/notes.html")); //localhost:3000/notes extension
 });
 
 router.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname__, "../public/index.html")); //localhost:3000 extension
+  res.sendFile(path.join(__dirname, "../public/index.html")); //localhost:3000 extension
 });
 
 module.exports = router;
